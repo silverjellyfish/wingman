121780549f4c5c16628b4ceda81f541ff0c6b152
Lana <pomme@skooge.com>
2025-12-01 14:02:41 -0600

    remove luggage from user schema

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -18,8 +18,6 @@ const userSchema = new mongoose.Schema(
     gender: { type: String, enum: ["male", "female", "other"] },
     emergencyContact: { type: String },
     activeFlights: [{ type: mongoose.Schema.Types.ObjectId, ref: "Flight" }],
-    numCheckedBags: { type: Number, default: 0 },
-    numCarryOnBags: { type: Number, default: 1 },
   },
   { timestamps: true }
 );
