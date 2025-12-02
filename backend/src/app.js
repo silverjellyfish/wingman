bdedbf8730d363762b76a6886b3bb051f31b6c5d
LeoDavynci <vincelin360@yahoo.com>
2025-12-01 23:25:54 -0600

    should be fixed for deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,39 +12,39 @@ const app = express();
 // STEPS FOR LOCAL TESTING
 // COMMENT THE FOLLOWING OUT
 /////////////////////////////////////////////////// BELOW THIS POINT
-// const allowedOrigins = [
-//   "https://wingman-for-you.vercel.app",
-//   "https://wingman-git-deploy-m1chelle7s-projects.vercel.app",
-// ];
+const allowedOrigins = [
+   "https://wingman-for-you.vercel.app",
+   "https://wingman-git-deploy-m1chelle7s-projects.vercel.app",
+];
 
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       if (!origin) return callback(null, true);
+app.use(
+   cors({
+      origin: function (origin, callback) {
+         if (!origin) return callback(null, true);
 
-//       if (allowedOrigins.includes(origin)) {
-//         return callback(null, true);
-//       } else {
-//         return callback(new Error("Not allowed by CORS"), false);
-//       }
-//     },
-//     credentials: true,
-//   })
-// );
+         if (allowedOrigins.includes(origin)) {
+            return callback(null, true);
+         } else {
+            return callback(new Error("Not allowed by CORS"), false);
+         }
+      },
+      credentials: true,
+   })
+);
 
 /////////////////////////////////////////////////// ABOVE THIS POINT
 
 // AND UNCOMMENT THE FOLLOWING:
-app.use(
-   cors({
-      origin: "http://localhost:5173",
-   })
-);
+// app.use(
+//    cors({
+//       origin: "http://localhost:5173",
+//    })
+// );
 ///////////////////////////////////////////////////
 
 // Middleware
-app.use(express.json({ limit: '10mb' })); // Increase limit for base64 images
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: "10mb" })); // Increase limit for base64 images
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use("/api", routes);
 app.use(errorHandler);
 
